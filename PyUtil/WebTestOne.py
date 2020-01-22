296c815b83925193e5896426599f9c0e865ddb44
pandey1ashish <ashishpandey.list@gmail.com>
2020-01-23 01:06:54 +0530

    New updates for GET/POST methods

--- PyUtil/WebTestOne.py
+++ PyUtil/WebTestOne.py
@@ -14,6 +14,22 @@ def user(name):
     return render_template("table.html", content=name)
 
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login_form.html")
+    elif request.method == 'POST':
+        text = request.form['first_name']
+        return "Form submitted! %s"%text
+    
 
+@app.route('/usr_login', methods=['GET', 'POST'])
+def usr_login():
+    username = request.form['username']
+    print(username)
+    password = request.form['password']
+    print(password)
+    
+    
 if __name__ == "__main__":
     app.run()
